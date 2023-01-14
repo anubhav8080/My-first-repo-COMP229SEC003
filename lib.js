c10770312c80c9650f18b17fa8ea36707488d2da
Anubhav Chauhan <anubhav8080@gmail.com>
2023-01-14 10:20:32 -0500

    my first commit in 2nd class

---
+++ lib.js
@@ -0,0 +1,8 @@
+export function halfOf(x){
+    return x/2;
+}
+
+
+export function multiple(x,y){
+    return x*y;
+}
---
+++ main.js
@@ -0,0 +1,4 @@
+import doSomething from "./doSomething";
+import {halfOf, multiple} from "./lib.js";
+console.log(halfOf(100));
+console.log(multiple(100,5));
